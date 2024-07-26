88ff93381cf979f2bee6de78418dc4650ad75c34
Johannes Eski <johannes.eski.alfame@ahti.cloud>
2024-07-26 11:50:15 +0300

    Refactor TrackingResultsTable component to highlight different values in table headers

--- dbtool-frontend/components/Tracking/TrackingResultsTable.tsx
+++ dbtool-frontend/components/Tracking/TrackingResultsTable.tsx
@@ -13,6 +13,8 @@ const TrackingResultsTable = ({ title, titleColor, rows }: Props) => {
   const data: JsonRow[] = rows.map(row => JSON.parse(row));
   const allKeys = Array.from(new Set(data.flatMap(Object.keys)));
 
+  const everyDataIsSame = (key: string) => data.every((val, i, arr) => val[key] === arr[0][key]);
+
   return (
     <Flex direction="column" mb={5}>
       <Text fontSize="sm" px={2} py={2} as="b" color={titleColor}>
@@ -23,9 +25,14 @@ const TrackingResultsTable = ({ title, titleColor, rows }: Props) => {
         <Table>
           <Thead>
             <Tr>
-              {allKeys.map(key => (
-                <Th key={key}>{key}</Th>
-              ))}
+              {allKeys.map(key => {
+                const color = everyDataIsSame(key) ? 'white' : 'blue.400';
+                return (
+                  <Th key={key} color={color}>
+                    {key}
+                  </Th>
+                );
+              })}
             </Tr>
           </Thead>
           <Tbody>
